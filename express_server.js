1cd248b6144e66b75c1753178b8e4ce47d432fff
RongYangAriel <yang.ariel@hotmail.com>
2020-11-10 21:05:31 -0500

    update edit get urls_show function

--- express_server.js
+++ express_server.js
@@ -37,7 +37,8 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   //res.render("urls_show", templateVars);
   const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  res.render("urls_show", templateVars);
+  // res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
